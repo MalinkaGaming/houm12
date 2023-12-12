0a4980ba89d57b1f65ac08f54791b123fe5a0838
MalinkaGaming <anutka512@yandex.ru>
2023-12-13 01:10:17 +0300

    домашка12 вроде работает

--- src/Book.java
+++ src/Book.java
@@ -1,28 +1,49 @@
+import java.util.Objects;
+
 public class Book {
-    private String name;
+    private final String bookName;
+    private final Author author;
     private int year;
-    private String author;
 
-    public Book(String name, int year, String author) {
-        this.name = name;
-        this.year = year;
-        this.author = author;
-    }
-
-    public String getName() {
-        return this.name;
+    Book(String inBookName,Author inAutor,int inYear){
+        bookName=inBookName;
+        author = inAutor;
+        year = inYear;
     }
 
     public int getYear() {
-        return this.year;
+        return year;
     }
 
-    public String getAuthor() {
-        return this.author;
+    public Author getAuthor() {
+        return author;
     }
 
-    public void setYear(int Year) {
-        this.year = Year;
+    public String getBookName() {
+        return bookName;
+    }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги = \"" + bookName +
+                "\", автор = " + author.toString() +
+                ", год издания = " + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, author);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,22 @@
+
 public class Main {
     public static void main(String[] args) {
-        String nameMetro = " Metro ";
-        int yearMetro = 2005;
-        String authorMetro = "Дмитрий Глуховский ";
+        Author firstAutor = new Author("Дмитрий", "Глуховский");
+        Author scndAutor = new Author("Евгений", "Водолазкин");
 
 
-        String nameLavr = " Lavr ";
-        int yearLavr = 2021;
-        String authorLavr = "Евгений Водолазкин ";
-
-
-        String[] names = {" Metro ", " Lavr "};
-        int[] year = {2005, 2021};
-        String[] author = {"Дмитрий Глуховский ", "Евгений Водолазкин "};
-
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(" Имя - " + names[i] + " год " + year[i] + " автор " + author[i]);
-        }
-        Book Metro = new Book(" Metro ", 2005, "Дмитрий Глуховский ");
-        System.out.println("Metro.name = " + Metro.getName());
-        System.out.println("Metro.year = " + Metro.getYear());
-        System.out.println("Metro.author = " + Metro.getAuthor());
-        Metro.setYear(2006);
-        System.out.println("metro.getYear () = " + Metro.getYear());
-        Book Lavr = new Book("Lavr", 2021, "Евгений Водолазкин");
-        System.out.println(" Lavr.name = " + Lavr.getName());
-        System.out.println(" Lavr.year = " + Lavr.getYear());
-        System.out.println("Lavr.author = " + Lavr.getAuthor());
-        Lavr.setYear(2022);
-        System.out.println("lavr.getYear () = " + Lavr.getYear());
-
-        String authorsNameLavr = " Евгений ";
-        String surnameAuthorsLavr = " Водолазкин ";
-
-        String authorsNameMetro = "Дмитрий";
-        String surnameMetro = "Глуховский ";
-
-        String[] authors = {" Евгений ", " Дмитрий "};
-        String[] surname = {" Водолазкин  ", " Глуховский "};
-
-        System.out.println(" Имя - " + authors + " фамилия " + surname);
-        Author Дмитрий = new Author("Дмитрий", "Глуховский");
-        System.out.println("Метро.authors = " + Дмитрий.getAuthors());
-        System.out.println("Метро.surname = " + Дмитрий.getSurname());
-
-        Author Евгений = new Author("Евгений", "Водолазкин");
-        System.out.println("Lavr.authors = " + Евгений.getAuthors());
-        System.out.println("Lavr.surname = " + Евгений.getSurname());
-
+        Book metro = new Book("Метро 2033", firstAutor, 2005);
+        System.out.println(metro);
+        Book lavr = new Book("Лавр", scndAutor, 2021);
+        System.out.println(lavr);
+        lavr.setYear(2022);
+        System.out.println(lavr);
+        //сравниваем
+        System.out.println(firstAutor.equals(scndAutor));
+        System.out.println(metro.equals(lavr));
+        //чекаем хэш
+        System.out.println(firstAutor.hashCode());
+        System.out.println(metro.hashCode());
+        System.out.println(metro.hashCode() == lavr.hashCode());
     }
 }
-
